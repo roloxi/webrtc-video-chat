e6adf671ac772ed5101b432df7a15abaafc4c2b1
joloz <jkuronen95@gmail.com>
2020-08-30 21:45:42 +0300

    Add basic functionality

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,20 @@ const io = require('socket.io')(server)
 
 io.on('connection', socket => {
   console.log(`${socket.id} connected`)
+  
+  socket.on('join', room => {
+    socket.join(room)
+    socket.room = room
+    socket.to(room).emit('join', socket.id)
+  })
+
+  socket.on('join-answer', id => {
+    socket.to(id).emit('join-answer', socket.id)
+  })
+
+  socket.on('signal', ({ to, data }) => {
+    socket.to(to).emit('signal', { from: socket.id, data })
+  })
 
   socket.on('disconnect', () => {
     console.log(`${socket.id} disconnected`)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react'
+import io from 'socket.io-client'
+import Peer from 'simple-peer'
+
+const socket = io('http://localhost:8000')
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [ streams, setStreams ]             = useState([])
+  const [ remoteStreams, setRemoteStreams ] = useState([])
+  const streamsRef                          = useRef()
+  const remoteStreamsRef                    = useRef()
+  const { current: peers }                  = useRef({})
+
+  // initialize streamsRefs to match states on each render
+  streamsRef.current = streams
+  remoteStreamsRef.current = remoteStreams
+
+  // create peer connection for socket
+  function initPeer(id, initiator = false) {
+    const peer = new Peer({
+      initiator,
+      trickle: false,
+      streams: streamsRef.current
+    })
+    peer.on('signal', data => {
+      socket.emit('signal', { to: id, data })
+    })
+    peer.on('stream', stream => {
+      setRemoteStreams(prevStreams => [...prevStreams, stream])
+    })
+    peers[id] = peer
+  }
+
+  async function getStream() {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+    setStreams(prevStreams => [...prevStreams, stream])
+    Object.values(peers).forEach(peer => peer.addStream(stream))
+  }
+  
+  // component mount effect
+  useEffect(() => {
+    // TODO: add dynamic rooms
+    socket.emit('join', 1) // join room 1
+
+    // add socket event listeners
+    socket.on('join', id => {
+      initPeer(id, true)
+      socket.emit('join-answer', id)
+    })
+    socket.on('join-answer', id => initPeer(id))
+    socket.on('signal', ({ from, data }) => peers[from].signal(data))
+
+    // initialize video stream
+    getStream()
+  }, [])
+
+  // render
+  return <div className="App">
+    { streams && streams.map(stream => <Video key={stream.id} stream={stream}/>) }
+    { remoteStreams && remoteStreams.map(stream => <Video key={stream.id} stream={stream}/>) }
+  </div>
 }
 
-export default App;
+
+// Video component
+
+function Video({ stream }) {
+  const ref = useRef()
+
+  // update video ref when stream changes
+  useEffect(() => {
+    ref.current.srcObject = stream
+  }, [stream])
+
+  return <video ref={ref} autoPlay />
+}
+
+export default App
